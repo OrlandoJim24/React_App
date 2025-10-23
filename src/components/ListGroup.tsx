c665a625166a3c5b8f73076126ad024674e8a3dd
OrlandoJim24 <orlandojim2023@gmail.com>
2025-10-23 12:05:06 -0400

    Initial commit: React + TS list app

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,24 @@
+import type { MouseEvent } from "react";
+
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item Found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
